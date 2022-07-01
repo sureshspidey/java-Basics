85f54ff517498c97ee10f4779c2c2e15c22cbb3b
sureshspidey <92290570+sureshspidey@users.noreply.github.com>
2022-07-01 19:25:26 +0530

    Add files via upload

---
+++ addTwoNum.java
@@ -0,0 +1,17 @@
+package imp.javapractice;
+import java.util.Scanner;
+public class addTwoNum {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter any two nums . I will perform addition\t=\t");
+		int a=scan.nextInt();
+		int b=scan.nextInt();
+	    System.out.printf("%d+%d=%d",a,b,a+b);
+	    scan.close();
+		
+		
+		// TODO Auto-generated constructor stub
+	}
+
+}
