07e0133061cf07359d9d74b02b1fa30b49a9d843
sureshspidey <92290570+sureshspidey@users.noreply.github.com>
2022-07-01 19:24:33 +0530

    Add files via upload

---
+++ Dog.java
@@ -0,0 +1,15 @@
+package com.dog;
+
+public class Dog {
+	public int a=1;
+	public int b=2;
+	public int c=a+b;
+
+	public static void main(String[] args) {
+		Dog add =new Dog();
+		System.out.println(add.c);
+		
+
+	}
+
+}
